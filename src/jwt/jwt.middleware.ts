7cc3427a4548fdbd1a008434afca790a47b918a3
jonghyeon <whdgus003@gmail.com>
2022-10-12 01:25:24 +0900

    add auth-decorator

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -13,12 +13,12 @@ export class JwtMiddleware implements NestMiddleware {
     if ('x-jwt' in req.headers) {
       const token = req.headers['x-jwt'];
       const decoded = this.jwtService.verify(token);
-      if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
-        try {
+      try {
+        if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
           const user = await this.usersService.findById(decoded['id']);
           req['user'] = user;
-        } catch (error) {}
-      }
+        }
+      } catch (error) {}
     }
     next();
   }
